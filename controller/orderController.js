1f5f0ff6210fa1f17c46abfbc6e790f89480e71c
Minuri Sewmini <minurisewmini11@gmail.com>
2025-07-27 23:11:40 +0530

    order validation created 😍😍

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,4 +1,5 @@
 import Order from "../models/order.js";
+import Product from "../models/product.js";
 import { isCustomer } from "./userController.js";
 
 export async function createOrder (req,res){
@@ -30,6 +31,45 @@ export async function createOrder (req,res){
         }
 
         const newOrderData =req.body
+
+        const newProductArray = []
+
+        // for (let i=0;i<newOrderData.orderItems.length;i++){
+        //     console.log(newOrderData.orderItems[i])
+        // }
+    for (let i=0;i<newOrderData.orderItems.length;i++){
+
+
+        const product = await Product.findOne({
+            productId : newOrderData.orderItems[i].productId
+        })
+
+        if (product == null){
+            res.json({
+                message : "Product with ID "+newOrderData.orderItems[i].productId+" not found"
+            })
+            return
+        }
+
+        newProductArray[i]={
+            productId : product.productId,
+            name : product.productName,
+            price : product.price,
+            quantity : newOrderData.orderItems[i].quantity,
+            image : product.images[0]
+        }
+        
+
+    }
+
+        console.log(newProductArray)
+        newOrderData.orderItems = newProductArray
+
+
+        console.log("Received orderItems:", newOrderData.orderItems);
+
+
+
         newOrderData.orderId = orderId
         newOrderData.email = req.user.email
 
